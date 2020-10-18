3a5bc13339e1721b54b7b5b8c0d634fb7cae4f8a
Sergio Bermudez <sbermudez@bestday.com>
2020-10-18 16:17:45 -0500

    delete clearConfigCache in ListImages

--- src/componentes/ListImages.jsx
+++ src/componentes/ListImages.jsx
@@ -1,9 +1,6 @@
-import { clearConfigCache } from 'prettier';
 import React from 'react';
 import Image from './Image';
 const ListImage = ({ myImages }) => {
-    console.log(myImages);
-
     return (  
         <div className="col-12 p-5 row">
             {myImages.map(myImage => (
